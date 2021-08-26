a4d3b063f1e7a5969cff75aea91f8a58c6467e17
14268 <1003043092@qq.com>
2021-08-26 11:23:50 +0800

    log学习

--- src/test/java/MyTest22.java
+++ src/test/java/MyTest22.java
@@ -3,6 +3,10 @@ import org.junit.Test;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author TF014268
@@ -10,6 +14,48 @@ import java.io.IOException;
  * @since 2021/7/28 0028 16:01
  */
 public class MyTest22 {
+    @Test
+    public void test2() throws IOException {
+        Logger log1 = Logger.getLogger("test1");
+        log1.setLevel(Level.INFO);
+
+//        Logger log2 = Logger.getLogger("test1");
+//        System.out.println(log1==log2);
+//        System.out.println("---------look---------");
+
+        Logger log3 = Logger.getLogger("test2");
+
+        //控制台控制器
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(Level.ALL);
+        log1.addHandler(ch);
+
+        //文件控制器
+        FileHandler fh = new FileHandler("E:\\GitLabMavenWorkSpace\\Tank\\Tank\\src\\test\\java\\myLog.log");
+//        fh.setLevel(Level.ALL);
+        log3.addHandler(fh);
+
+        log1.info("log1--info");
+        log3.info("log3--info");
+        System.out.println("------------打印了吗------------");
+
+        log1.fine("log1--fine");
+        log3.fine("log3--fine");
+    }
+
+
+    @Test
+    public void test1(){
+        Logger logger = Logger.getLogger(MyTest22.class.getName());
+        logger.setLevel(Level.ALL);
+
+        logger.info("这个info");
+        logger.warning("这个warning");
+//        logger.
+    }
+
+
+
     public static void main(String[] args) {
         BufferedImage read = null;
         try {
